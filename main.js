89078452f0a9b6f9c07e30889f033a07b7167d55
Koketso19 <bigshowbiggy73@gmail.com>
2024-12-17 03:34:08 +0200

    add

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 let board = [
-    ['','','']
-    ['','','']
-    ['','','']
+    ['X','O','O'],
+    ['O','O','X'],
+    ['X','X','0'],
 ];
 
-let playerq1 = 'x'
-let player2 = 'o'
+let playerq1 = 'X';
+let player2 = 'O';
 
 
 function setup(){
@@ -14,4 +14,16 @@ function setup(){
 
 function draw() {
     background(220);
+    let w = width / 3;
+    let h = height / 3;
+    
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j <3; j++){
+            let x = w * i;
+            let y = h * j;
+            let spot = board[i][j];
+            textSize(32);
+            Text(spot, x, y);
+        }
+    }
 }

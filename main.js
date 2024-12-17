8d845ebd0ae86fe915b75f80da5f1c5f114f47e8
Koketso19 <bigshowbiggy73@gmail.com>
2024-12-17 02:52:17 +0200

    creating board plus players fuction setup

---
+++ main.js
@@ -0,0 +1,17 @@
+let board = [
+    ['','','']
+    ['','','']
+    ['','','']
+];
+
+let playerq1 = 'x'
+let player2 = 'o'
+
+
+function setup(){
+    createCanvas(400, 400);
+}
+
+function draw() {
+    background(220);
+}

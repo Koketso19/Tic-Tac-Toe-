10e07a631149f26571c3c6408ca1fd7c278e8e3c
Koketso19 <bigshowbiggy73@gmail.com>
2024-12-17 12:43:52 +0200

    drawing O AND X, diagram this, randomly choose the first player at the start of round

--- main.js
+++ main.js
@@ -4,12 +4,10 @@ let board = [
     ['X','X','0'],
 ];
 
-let playerq1 = 'X';
-let player2 = 'O';
-
+let players = ['X', 'O'];
 
 function setup(){
-    createCanvas(400, 400);
+    createCanvas(1000, 1000);
 }
 
 function draw() {
@@ -17,13 +15,22 @@ function draw() {
     let w = width / 3;
     let h = height / 3;
     
-    for (let i = 0; i < 3; i++) {
-        for (let j = 0; j <3; j++){
-            let x = w * i;
-            let y = h * j;
+    for (let i = 0; j < 3; j++) {
+        for (let i = 0; i < 3; i++){
+            let x = w * i + w/2;
+            let y = h * j + h/2;
             let spot = board[i][j];
             textSize(32);
-            Text(spot, x, y);
+            strokeMeight(4);
+            if (spot == player[1]){
+                noFill();
+                ellipse(x,y,w/2);
+            } else if (spot == player[0]) {
+                let xr = w/4;
+                line(x-xr, y-xr, x + xr, y + xr);
+                line(x+xr, y-xr, x-xr, y + xr );
+            }
+            text(spot, x, y);
         }
     }
 }
